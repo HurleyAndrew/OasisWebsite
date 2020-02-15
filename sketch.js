3fd75f7088c6c773e30f4a43c6306dad008eec39
Andrew Hurley <hurleymandrew@gmail.com>
2020-02-15 01:53:40 -0500

    site finished v1

--- sketch.js
+++ sketch.js
@@ -1,170 +1,81 @@
-let sphereArr = [];
-let sphereCount = 50;
-let time = 0;
-let c;
-var isMobile = {
-  Android: function() {
-    return navigator.userAgent.match(/Android/i) ? true : false;
-  },
-  BlackBerry: function() {
-    return navigator.userAgent.match(/BlackBerry/i) ? true : false;
-  },
-  iOS: function() {
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
-  },
-  Windows: function() {
-    return navigator.userAgent.match(/IEMobile/i) ? true : false;
-  },
-  any: function() {
-    return (
-      isMobile.Android() ||
-      isMobile.BlackBerry() ||
-      isMobile.iOS() ||
-      isMobile.Windows()
-    );
-  }
-};
+let lightArr = [];
 
 function setup() {
-  var cnv = createCanvas(document.body.clientWidth, document.body.clientHeight);
-  //   console.log(document.body.clientHeight);
-  var x = (document.body.clientWidth - width) / 2;
-  var y = (document.body.clientHeight - height) / 2;
-  //   console.log(document.body.clientHeight);
-  //   console.log(height);
-  //   console.log(y);
+  var cnv = createCanvas(windowWidth - 50, windowHeight);
+
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+
   cnv.position(x, 0);
-  c = color("#F0F5F9");
-  //   background(255, 0, 200);
-  //   for (let i = 0; i < sphereCount; i++) {
-  //     sphereArr.push(new Sphere(mouseX, mouseY));
-  //   }
-  //   for (let i = 0; i < sphereCount; i++) {
-  //     sphereArr[i].display();
-  //   }
+
+  for (i = 0; i < 20; i++) {
+    lightArr.push(new LightObj(random(0, 5), random(0, 5)));
+  }
   noStroke();
+  // noLoop();
+  // background(0, 100);
+  // background(0);
+}
 
-  //   fill(c);
-  //   rect(0, 0, document.body.clientWidth, document.body.clientHeight);
-  background(c, 100);
-
-  if (isMobile.any()) {
-    console.log("ismobile");
-
-    noLoop();
-  } else {
-    loop();
+function draw() {
+  for (i = 0; i < 20; i++) {
+    lightArr[i].display();
+    // console.log(lightArr[i].xoff);
   }
 }
 
 function windowResized() {
-  resizeCanvas(document.body.clientWidth, document.body.clientHeight);
+  resizeCanvas(windowWidth - 50, windowHeight);
 }
 
-function draw() {
-  if (!isMobile.any()) {
-    //   background(240, 245, 0, 100);
-
-    if (sphereArr.length > 0) {
-      for (let i = 0; i < sphereArr.length; i++) {
-        //   console.log(sphereArr[i].getXCoord());
-        sphereArr[i].display();
-        sphereArr[i].update();
-        //   if (sphereArr[i].isDead) {
-        //     //   sphereArr.remove(i);
-        //     console.log("item killed: " + sphereArr[i].isDead);
-        //     sphereArr.splice(sphereArr.indexOf(i), 1);
-        //   } else {
-        //     console.log(sphereArr[i].getXCoord());
-        //     sphereArr[i].display();
-        //     sphereArr[i].update();
-        //   }
-      }
-    }
-    if (time % 4 == 0) {
-      // console.log("new pushed");
-      sphereArr.push(new Sphere(random(width), random(height)));
-    }
-    if (time % 15 == 0) {
-      background(240, 245, 249, 70);
-    }
-    background(240, 245, 249, 25);
-    ellipse(mouseX, mouseY, 15, 15);
-    time += 1;
-  }
-}
-
-// function mouseMoved() {
-//   if (!(sphereArr.length >= 2000)) {
-//     console.log(sphereArr.length);
-//     sphereArr.push(new Sphere(mouseX, mouseY));
-//   }
-// }
-function mouseClicked() {}
-
-class Sphere {
-  constructor(xHold, yHold) {
-    this.isDead = false;
-    this.x = xHold;
-    this.y = yHold;
-    this.size = random(1, 5);
-    this.decrementSize = random(0.005, 0.01);
-    this.scale = 2.0;
-    this.xoff = 0.0;
-    this.yoff = 0.0;
-    this.randomOption = random(0, 1);
-    this.opacity = 100;
-    // this.display();
+class LightObj {
+  constructor(offsetX, offsetY) {
+    this.xoff = offsetX;
+    this.yoff = offsetY;
+    this.xincrement = 0.001;
+    this.yincrement = 0.001;
+    this.randW = random(0, width * 2);
+    this.randH = random(0, height * 2);
+    this.randSize = random(5, 15);
   }
 
-  update() {
-    // this.x = noise(this.xoff) * width;
-    // this.y = noise(this.yoff) * height;
-
-    this.xoff = this.xoff + random(0.001, 0.01);
-    this.yoff = this.yoff + random(0.003, 1);
-
-    if ((this.randomOption >= 0) & (this.randomOption <= 0.25)) {
-      this.x += noise(this.xoff) * 2.0;
-      this.y += noise(this.yoff) * 2.0;
-    } else if ((this.randomOption > 0.25) & (this.randomOption <= 0.5)) {
-      this.x -= noise(this.xoff) * 2.0;
-      this.y -= noise(this.yoff) * 2.0;
-    } else if ((this.randomOption > 0.5) & (this.randomOption <= 0.75)) {
-      this.x += noise(this.xoff) * 2.0;
-      this.y -= noise(this.yoff) * 2.0;
-    } else {
-      this.x -= noise(this.xoff) * 2.0;
-      this.y += noise(this.yoff) * 2.0;
-    }
-
-    // this.x += noise(this.xoff) * 2.0;
-    // this.y += noise(this.yoff) * 2.0;
-    // this.x = noise(this.xoff) * 20.0;
-    // this.y = noise(this.yoff) * 20.0;
-    this.size -= this.decrementSize;
-
-    if (this.size <= 0) {
-      this.isDead = true;
-      //   this.size = random(1, 5);
-      //   this.randomOption = random(0, 1);
-      //   this.x = mouseX;
-      //   this.y = mouseY;
-      //   this.opacity = 100;
-    }
-    // this.opacity -= this.decrementSize * 100;
-  }
-  getXCoord() {
-    return this.x;
-  }
   display() {
-    if (!this.isDead) {
-      if (this.x > 50 && this.y > 50) {
-        fill(132, 164, 246, this.opacity);
-        ellipse(this.x, this.y, this.size, this.size);
-      } else {
-        //   console.log(this.x);
-      }
-    }
+    // if (time % 5 == 0) {
+    let nx = noise(this.xoff) * this.randW;
+    let ny = noise(this.yoff) * this.randH;
+
+    // With each cycle, increment xoff
+    this.xoff += this.xincrement;
+    this.yoff += this.yincrement;
+
+    // Draw the ellipse at the value produced by perlin noise
+    // make a mask that follows the center of the light
+
+    fill(0);
+    // ellipse(this.nx, this.ny, 30, 30);
+    // ellipse(nx, ny, 30, 30);
+
+    erase();
+    fill(0);
+    ellipse(nx, ny, 30, 30);
+    erase();
+    fill(255);
+    ellipse(nx, ny, 10, 10);
+    noErase();
+    fill(132, 164, 246, 25);
+    ellipse(nx, ny, 5, 5);
+    fill(132, 164, 246, 50);
+    ellipse(nx, ny, this.randSize, this.randSize);
+    fill(132, 164, 246);
+    ellipse(nx, ny, 4, 4);
+    // console.log("x: " + nx + " y: " + ny);
+    noErase();
+
+    // }
+    // if (time % 10 == 5) {
+    // erase();
+    // rect(nx, ny, 20, 20);
+    // noErase();
+    // }
   }
 }
---
+++ sketchOld.js
@@ -0,0 +1,171 @@
+let sphereArr = [];
+let sphereCount = 50;
+let time = 0;
+let c;
+var isMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i) ? true : false;
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i) ? true : false;
+  },
+  any: function() {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Windows()
+    );
+  }
+};
+
+function setup() {
+  var cnv = createCanvas(document.body.clientWidth, document.body.clientHeight);
+  //   console.log(document.body.clientHeight);
+  var x = (document.body.clientWidth - width) / 2;
+  var y = (document.body.clientHeight - height) / 2;
+  //   console.log(document.body.clientHeight);
+  //   console.log(height);
+  //   console.log(y);
+  cnv.position(x, 0);
+  c = color("#F0F5F9");
+  //   background(255, 0, 200);
+  //   for (let i = 0; i < sphereCount; i++) {
+  //     sphereArr.push(new Sphere(mouseX, mouseY));
+  //   }
+  //   for (let i = 0; i < sphereCount; i++) {
+  //     sphereArr[i].display();
+  //   }
+  noStroke();
+
+  //   fill(c);
+  //   rect(0, 0, document.body.clientWidth, document.body.clientHeight);
+  background(c, 100);
+
+  if (isMobile.any()) {
+    console.log("ismobile");
+
+    noLoop();
+  } else {
+    loop();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(document.body.clientWidth, document.body.clientHeight);
+}
+
+function draw() {
+  if (!isMobile.any()) {
+    //   background(240, 245, 0, 100);
+
+    if (sphereArr.length > 0) {
+      for (let i = 0; i < sphereArr.length; i++) {
+        //   console.log(sphereArr[i].getXCoord());
+        sphereArr[i].display();
+        sphereArr[i].update();
+        // if (sphereArr[i].isDead) {
+        //   //   sphereArr.remove(i);
+        //   // console.log("item killed: " + sphereArr[i].isDead);
+        //   sphereArr.splice(sphereArr.indexOf(i), 1);
+        // } else {
+        //   // console.log(sphereArr[i].getXCoord());
+        //   sphereArr[i].display();
+        //   sphereArr[i].update();
+        // }
+      }
+    }
+    if (time % 4 == 0) {
+      // console.log("new pushed");
+      sphereArr.push(new Sphere(random(width), random(height)));
+    }
+
+    // if (time % 15 == 0) {
+    //   background(240, 245, 249, 0);
+    // }
+    background(240, 245, 249, 15);
+    ellipse(mouseX, mouseY, 15, 15);
+    time += 1;
+  }
+}
+
+// function mouseMoved() {
+//   if (!(sphereArr.length >= 2000)) {
+//     console.log(sphereArr.length);
+//     sphereArr.push(new Sphere(mouseX, mouseY));
+//   }
+// }
+function mouseClicked() {}
+
+class Sphere {
+  constructor(xHold, yHold) {
+    this.isDead = false;
+    this.x = xHold;
+    this.y = yHold;
+    this.size = random(1, 5);
+    this.decrementSize = random(0.005, 0.01);
+    this.scale = 2.0;
+    this.xoff = 0.0;
+    this.yoff = 0.0;
+    this.randomOption = random(0, 1);
+    this.opacity = 100;
+    // this.display();
+  }
+
+  update() {
+    // this.x = noise(this.xoff) * width;
+    // this.y = noise(this.yoff) * height;
+
+    this.xoff = this.xoff + random(0.001, 0.01);
+    this.yoff = this.yoff + random(0.003, 1);
+
+    if ((this.randomOption >= 0) & (this.randomOption <= 0.25)) {
+      this.x += noise(this.xoff) * 2.0;
+      this.y += noise(this.yoff) * 2.0;
+    } else if ((this.randomOption > 0.25) & (this.randomOption <= 0.5)) {
+      this.x -= noise(this.xoff) * 2.0;
+      this.y -= noise(this.yoff) * 2.0;
+    } else if ((this.randomOption > 0.5) & (this.randomOption <= 0.75)) {
+      this.x += noise(this.xoff) * 2.0;
+      this.y -= noise(this.yoff) * 2.0;
+    } else {
+      this.x -= noise(this.xoff) * 2.0;
+      this.y += noise(this.yoff) * 2.0;
+    }
+
+    // this.x += noise(this.xoff) * 2.0;
+    // this.y += noise(this.yoff) * 2.0;
+    // this.x = noise(this.xoff) * 20.0;
+    // this.y = noise(this.yoff) * 20.0;
+    this.size -= this.decrementSize;
+
+    if (this.size <= 0) {
+      this.isDead = true;
+      //   this.size = random(1, 5);
+      //   this.randomOption = random(0, 1);
+      //   this.x = mouseX;
+      //   this.y = mouseY;
+      //   this.opacity = 100;
+    }
+    // this.opacity -= this.decrementSize * 100;
+  }
+  getXCoord() {
+    return this.x;
+  }
+  display() {
+    if (!this.isDead) {
+      if (this.x > 50 && this.y > 50) {
+        fill(132, 164, 246, this.opacity);
+        ellipse(this.x, this.y, this.size, this.size);
+      } else {
+        //   console.log(this.x);
+      }
+    }
+  }
+}
