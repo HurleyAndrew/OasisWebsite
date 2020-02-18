62c83714fc4ddc7f84b662563e5c9f1d960e1275
Andrew Hurley <hurleymandrew@gmail.com>
2020-02-17 20:32:23 -0500

    draft versions of applications

---
+++ meditationApp.js
@@ -0,0 +1,91 @@
+var serial; // variable to hold an instance of the serialport library
+var portName = "/dev/tty.usbmodem14201"; // fill in your serial port name here
+var inData; // variable to hold the input data from Arduino
+var outData = 0; // variable to hold the output data to Arduino
+
+function setup() {
+  var cnv = createCanvas(windowWidth, windowHeight);
+
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+
+  //set up communication port
+  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial.on("list", printList); // set a callback function for the serialport list event
+  serial.on("connected", serverConnected); // callback for connecting to the server
+  serial.on("open", portOpen); // callback for the port opening
+  serial.on("data", serialEvent); // callback for when new data arrives
+  serial.on("error", serialError); // callback for errors
+  serial.on("close", portClose); // callback for the port closing
+
+  serial.list(); // list the serial ports
+  serial.open(portName); // open a serial port
+
+  cnv.position(x, 0);
+  noStroke();
+  background(0);
+}
+
+function draw() {
+  background(255, 255, 255, 5);
+  fill(100, 45, 67, inData / 10);
+  // push();
+
+  ellipse(width / 2, height / 2, inData * 1.5, inData * 1.5);
+  // pop();
+  // console.log(inData);
+  // text("BRIGHTNESS LEVEL: " + inData, 30, 50); // displaying the input
+
+  //
+  // outData = rightBrightness;
+  // serial.write(outData);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+// --white: #f5f8fc;
+// --red: #d2290a;
+// --light-blue: #c0d1fc;
+// --medium-blue: #84a4f6;
+// --dark-blue: #304fa1;
+// --darker-blue: #283e82;
+// --light-gray: #dadfe8;
+// --medium-gray: #9dacca;
+// --dark-gray: #111e47;
+
+/*
+References for these codes:
+https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-input-to-the-p5-js-ide/
+https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-input-to-the-p5-js-ide/
+*/
+
+// Following functions print the serial communication status to the console for debugging purposes
+
+function printList(portList) {
+  // portList is an array of serial port names
+  for (var i = 0; i < portList.length; i++) {
+    // Display the list the console:
+    print(i + " " + portList[i]);
+  }
+}
+
+function serverConnected() {
+  print("connected to server.");
+}
+
+function portOpen() {
+  print("the serial port opened.");
+}
+
+function serialEvent() {
+  inData = Number(serial.read());
+}
+
+function serialError(err) {
+  print("Something went wrong with the serial port. " + err);
+}
+
+function portClose() {
+  print("The serial port closed.");
+}
